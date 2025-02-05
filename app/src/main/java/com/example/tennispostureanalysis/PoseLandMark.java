3f77ed9b2f73ad0cde4e8de3d1d8535f62acdc2c
Brian <144963829+Briantorralva@users.noreply.github.com>
2025-02-05 15:40:44 -0800

    Added MediaPipe Pose Estimation Model
    -Added Pose Estimation Model (Capture Page_

---
+++ app/src/main/java/com/example/tennispostureanalysis/PoseLandMark.java
@@ -0,0 +1,34 @@
+package com.example.tennispostureanalysis;
+
+public class PoseLandMark {
+    float x,y, visible;
+    PoseLandMark(float x, float y, float visible) {
+        this.x = x;
+        this.y = y;
+        this.visible = visible;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setVisible(float visible) {
+        this.visible = visible;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getVisible() {
+        return visible;
+    }
+}
