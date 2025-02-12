679f95a08f849b2bb61f604ed7f67d32138ecb88
caroalex <caroalex29@gmail.com>
2025-02-12 15:41:44 -0800

    Added Text And UserIcon
    Added "Welcome User" and UserIcon, still need to make the onClick for it

--- app/src/main/java/com/example/tennispostureanalysis/FeedbackPage.java
+++ app/src/main/java/com/example/tennispostureanalysis/FeedbackPage.java
@@ -52,10 +52,10 @@ public class FeedbackPage extends AppCompatActivity {
             }
         });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        });*/
     }
 }
